f791ebb87473d8372e4bc14dfcb8335f47e4a900
DLWangsan <1329864438@qq.com>
2018-08-02 10:21:15 +0800

    First commit.

---
+++ app/src/main/java/com/tiantian/dalaowangsan/tiantianweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tiantian.dalaowangsan.tiantianweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
